951145982b6799ef84f0d90133b86ff1a20a8329
Erwan31 <spilmonterwan@gmail.com>
2020-08-17 12:13:36 +0100

    Basic formfield compo

--- src/components/formOne/index.js
+++ src/components/formOne/index.js
@@ -1,4 +1,5 @@
 import React,{ Component } from 'react';
+import FormField from '../../utils/formFields';
 
 class FormOne extends Component {
 
--- src/utils/formFields.js
+++ src/utils/formFields.js
@@ -2,4 +2,29 @@ import React from 'react';
 
 const FormField = ({formData}) => {
 
+    const renderTemplate = () => {
+        let formTemplate = null;
+
+        switch(formData.element){
+            case('input'):
+                formTemplate = (
+                    <input type="text"/>
+                )
+            break;
+
+            default:
+                formTemplate = null;
+            break;
+        }
+
+        return formTemplate
+    }
+
+    return(
+        <>
+            {renderTemplate()}
+        </>
+    )
 }
+
+export default FormField;
